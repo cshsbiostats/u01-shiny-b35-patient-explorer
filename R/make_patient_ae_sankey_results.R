1d649ae19cbebf2c5ba124e464e3e204b408dfb7
mluu921 <mluu921@users.noreply.github.com>
2023-08-18 11:57:17 -0700

    initial commit

---
+++ R/make_patient_ae_sankey_results.R
@@ -0,0 +1,203 @@
+calculate_ti <- function(x) {
+  
+  x <- sort(x, decreasing = T)
+  ti <- NULL
+  
+  for (i in 1:length(x)) {
+    ti[i] <- x[i] * prod((x[1:i-1] + 1)^-1)
+  }
+  
+  return(sum(ti))
+}
+
+make_patient_ae_sankey_results <- \(data, selected_treatment, selected_ae, selected_timeframe, selected_grade) {
+  
+  
+  # selected_treatment <- 'Anastrozole'
+  # selected_ae <- 'Arthralgia (joint pain)'
+  # selected_timeframe <- c(6, 60)
+  # selected_grade <- '5'
+  
+  response_order <- c(
+    '0-1',
+    '2',
+    '3',
+    '4',
+    '5',
+    'Off Treatment'
+  )
+  
+  data <- local({
+    data <- data |>
+      mutate(ncycle = ncycle * 6) |> 
+      mutate(ncycle_label = glue::glue('{ncycle} M'))
+    
+    data <- data |>
+      filter(ncycle <= 60)
+    
+    data <- data |>
+      filter(trt == selected_treatment) |>
+      group_by(patientid) |>
+      filter(
+        any(
+          ncycle == selected_timeframe[1] &
+            ae == selected_ae &
+            ae_grade == selected_grade
+        )
+      ) |>
+      ungroup() |>
+      filter(
+        ncycle >= selected_timeframe[1] &
+          ncycle <= selected_timeframe[2]
+      ) |>
+      filter(ae == selected_ae)
+    
+  })
+  
+  if(nrow(data) == 0) {
+    return(FALSE)
+  }
+  
+  ti_hist <- local({
+    plot_data <- data %>%
+      filter(ae_grade != 'Off Treatment') %>%
+      group_by(patientid) %>%
+      mutate(grade = case_when(ae_grade == '0-1' ~ '0',
+                               TRUE ~ ae_grade),
+             grade = as.numeric(grade))  %>%
+      summarise(ti = calculate_ti(grade)) |> 
+      ungroup()
+    
+    stats <- plot_data |> 
+      skimr::skim(ti) |> 
+      as_tibble() |> 
+      mutate(across(numeric.mean:numeric.p100, \(x) scales::label_number(.01)(x)))
+
+    plot <- ggplot(plot_data, aes(x = ti)) +
+      geom_histogram() +
+      theme_bw(base_size = 15) +
+      scale_x_continuous(limits = c(0, 6), breaks = seq(0, 6, .5)) +
+      labs(
+        x = 'Toxicity Index (TI)',
+        y = 'n',
+        caption = glue::glue(
+          'Mean (SD): {stats$numeric.mean} ({stats$numeric.sd})\nMedian [IQR]: {stats$numeric.p50} [{stats$numeric.p25}, {stats$numeric.p75}]'
+        )
+      )
+    
+    plot
+    
+  })
+
+  grade_duration <- local({
+    
+    plot_data <- data %>%
+      filter(
+        ae_grade != 'Off Treatment'
+      ) %>%
+      group_by(ae_grade, ncycle) %>%
+      count() %>%
+      mutate(
+        ncycle = as_factor(ncycle),
+        ae_grade = paste0('Grade ', ae_grade)
+      ) %>%
+      ungroup() |> 
+      mutate(
+        label = glue::glue('{ae_grade} - {ncycle} M')
+      )
+    
+    ggplot(plot_data, aes(x = n, y = reorder(label, n))) + 
+      geom_col() + 
+      theme_bw(base_size = 15) + 
+      labs(x = 'n', y = NULL)
+  })
+  
+  
+  ##### need error handling on whether there is any data
+  
+  total_patients <- data |>
+    pull(patientid) |>
+    unique() |>
+    length()
+  
+  sankey <- local({
+    
+    plot_data <- data |>
+      select(-ncycle) |>
+      pivot_wider(names_from = ncycle_label, values_from = ae_grade,
+                  values_fill = 'Off Treatment')
+    
+    plot_data <- plot_data |>
+      make_long(4:ncol(plot_data))
+    
+    grade_colors <- c(
+      "0-1" = "#1F77B4FF",
+      "2" = "#2CA02CFF",
+      "3" = "#BCBD22FF",
+      "4" = "#FF7F0EFF",
+      "5" = "#D62728FF",
+      "Off Treatment" = "#7F7F7FFF"
+    )
+    
+    plot <- ggplot(
+      plot_data,
+      aes(
+        x = x,
+        next_x = next_x,
+        node = node,
+        next_node = next_node,
+        fill = factor(node)
+      )
+    ) +
+      geom_sankey(flow.alpha = .3) +
+      theme_sankey(base_size = 20) +
+      theme(
+        legend.position = 'bottom',
+        legend.title = element_blank(),
+        axis.text.x = element_text(face = 'bold'),
+        axis.title = element_blank()
+      ) +
+      scale_color_manual(values = grade_colors) +
+      scale_fill_manual(values = grade_colors) +
+      labs(
+        title = selected_ae,
+        subtitle = glue::glue('{selected_treatment}, n = {total_patients}')
+      )
+    
+    plot
+    
+  })
+  
+  summary_description <- local({
+    res <- data |>
+      filter(ncycle == selected_timeframe[2]) |>
+      count(ae_grade) |>
+      mutate(ae_grade = factor(ae_grade, levels = response_order)) |>
+      arrange(ae_grade) |>
+      mutate(
+        prop = n / sum(n),
+        prop = scales::percent_format()(prop)
+      )
+
+    summary_res <- glue::glue_data(res, 'A total of {n} ({prop}) of the patients responded "{ae_grade}". ') |>
+      glue::glue_collapse(sep = '<br>')
+
+
+    summary_description <- glue::glue(
+      'Among a total of {total_patients} patients who responded "{selected_grade}" for "{selected_ae}" at {selected_timeframe[1]} M.',
+      'By the {selected_timeframe[2]} M timepoint',
+
+      '{summary_res}',
+      .sep = '<br><br>'
+    )
+  })
+  
+  tibble::lst(
+    sankey,
+    summary_description,
+    ti_hist,
+    grade_duration
+  )
+  
+}
+
---
+++ app.R
@@ -0,0 +1,141 @@
+library(tidyverse)
+library(shiny)
+library(bslib)
+library(ggsankey)
+library(patchwork)
+library(shinyFeedback)
+
+data <- read_csv(here::here('data/u01_b35_data.csv'),
+                 show_col_types = FALSE)
+
+trt_options <- data |> pull(trt) |> unique() |> sort()
+
+ae_options <- data |> 
+  pull(ae) |> 
+  unique() |> 
+  sort()
+
+grade_options <- data |> 
+  pull(ae_grade) |> 
+  unique() |> 
+  sort()
+
+main <- layout_sidebar(
+  fillable = TRUE,
+  sidebar = sidebar(
+    selectInput(
+      inputId = 'select_trt',
+      label = '1. Select Treatment',
+      choices = trt_options
+    ),
+    selectInput(
+      inputId = 'select_ae',
+      label = '2. Select AE',
+      choices = ae_options
+    ),
+    selectInput(
+      inputId = 'select_grade',
+      label = '3. Select Grade',
+      choices = grade_options
+    ),
+    sliderInput(
+      inputId = 'select_timeframe',
+      label = '4. Select Timeframe',
+      min = 6,
+      max = 60,
+      value = c(6, 36),
+      step = 6
+    ),
+    actionButton(
+      'btn_ae_visualize',
+      'Visualize',
+      icon = icon(name = 'chart-bar', lib = 'font-awesome')
+    ),
+    downloadButton(
+      'report',
+      'Download Report',
+      icon = icon(name = 'chart-bar', lib = 'font-awesome')
+    )
+  ),
+  card(
+    card_header('Sankey Diagram'),
+    card_body(class = "p-0",
+              plotOutput('sankey_plot')),
+    card_body(htmlOutput('summary_descr'), ),
+    full_screen = TRUE,
+    fill = FALSE
+  ),
+  layout_column_wrap(
+    card(card_header('Grade Duration'),
+         card_body(class = "p-0",
+                   plotOutput('grade_duration')), full_screen = TRUE),
+    card(card_header('Toxicity Index'),
+         card_body(class = "p-0",
+                   plotOutput('ti_hist')), full_screen = TRUE),
+    width = 1 / 2
+  )
+)
+
+ui <- page_fillable(
+  main
+)
+
+shinyApp(ui, function(input, output) {
+  
+  results <- eventReactive(input$btn_ae_visualize, {
+    
+    out <- make_patient_ae_sankey_results(
+      data = data,
+      selected_treatment = input$select_trt,
+      selected_ae = input$select_ae,
+      selected_timeframe = input$select_timeframe,
+      selected_grade = input$select_grade
+    )
+    
+  })
+  
+  observeEvent(input$btn_ae_visualize, {
+    
+    req(results())
+    
+
+    
+    output$sankey_plot <- renderPlot({
+      
+      results()$sankey
+      
+    })
+    
+    output$ti_hist <- renderPlot({
+      
+      results()$ti_hist
+      
+    })
+    
+    output$summary_descr <- renderPrint({
+
+      results()$summary_description
+    })
+    
+    output$grade_duration <- renderPlot({
+      
+      results()$grade_duration
+    })
+    
+  })
+  
+  output$report <- downloadHandler(
+    filename = \(x) {paste0('pro_ctcae_ae_sankey_', Sys.Date(), '.pdf')},
+    content = function(file) {
+
+      rmarkdown::render(
+        'explore_qol_template.Rmd',
+        output_file = file,
+        params = list('results' = results()),
+        envir = new.env(parent = globalenv())
+      )
+
+    }
+  )
+
+})
